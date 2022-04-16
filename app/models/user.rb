71d3c7e2dedcf13634ecd6eb31f1213041eeae7f
Tad Vela <t.vela19@yahoo.com>
2022-04-16 19:04:12 -0400

    one to many association between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :articles, foreign_key: "id"
+
 	validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum:25} 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, 
