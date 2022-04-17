9c8006f2ee44293ada7eba288e2039f08adf1c31
Tad Vela <t.vela19@yahoo.com>
2022-04-17 09:34:15 -0400

    secure password with bcrypt and has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase }
 	has_many :articles, foreign_key: "id"
 
 	validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum:25} 
@@ -7,5 +8,8 @@ class User < ApplicationRecord
 									uniqueness: { case_sensitive: false }, 
 									length: { minimum: 3, maximum: 70 },
 									format: { with: VALID_EMAIL_REGEX }
+
+	has_secure_password
+
 
 end
